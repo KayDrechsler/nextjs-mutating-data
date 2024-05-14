c047ead5b55e21a328953ebef88753b7d6c649e1
KayDrechsler <kayzah@gmail.com>
2024-05-14 09:28:39 +0200

    + Dynamic Metadata, * Loading unknown images.

--- app/page.js
+++ app/page.js
@@ -3,6 +3,21 @@ import { Suspense } from 'react';
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+// export const metadata = {
+//     title: 'Browse all our X posts',
+//     description: 'Browse and share amazing posts.',
+// }
+
+export async function generateMetadata() {
+    const posts = await getPosts();
+    const numberOfPosts = posts.length;
+
+    return {
+        title: `Browse all our ${numberOfPosts} posts`,
+        description: 'Browse and share amazing posts.',
+    };
+};
+
 async function LatestPosts() {
     const latestPosts = await getPosts(2);
     return <Posts posts={latestPosts} />;
